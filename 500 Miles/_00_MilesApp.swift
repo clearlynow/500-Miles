0451fefcdf717cded5470405bbde0ba95c74f3de
clearlynow <59779189+clearlynow@users.noreply.github.com>
2020-12-21 09:27:33 -0700

    Initial Commit

---
+++ 500 Miles/_00_MilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00_MilesApp.swift
+//  500 Miles
+//
+//  Created by Alison Gorman on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct _00_MilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
